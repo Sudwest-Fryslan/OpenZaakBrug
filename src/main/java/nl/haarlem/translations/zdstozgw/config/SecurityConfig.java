0becc0efb5cb4612465283a2f424d266781cd09c
David van Hussel <dhussel@haarlem.nl>
2023-05-21 18:03:00 +0200

    [FEATURE] Added basic auth to logging API

---
+++ src/main/java/nl/haarlem/translations/zdstozgw/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package nl.haarlem.translations.zdstozgw.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+    @Value("${api.username:ozb}")
+    private String username;
+
+    @Value("${api.password:ozb}")
+    private String password;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+            .csrf().disable()
+            .authorizeRequests().antMatchers("/api/**")
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+        throws Exception
+    {
+        auth.inMemoryAuthentication()
+            .withUser(username)
+            .password("{noop}"+password)
+            .roles("USER");
+    }
+}
