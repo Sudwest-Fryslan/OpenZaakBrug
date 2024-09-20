e69d97ba4b127e42a3509350665ba80a8e118c7e
alisihab <sihab.akcan@gmail.com>
2024-09-20 15:15:10 +0200

    Fix plsql syntax (#374)

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -13,24 +13,24 @@ INSERT INTO emulate_parameter (parameter_name, parameter_description, parameter_
 SELECT 'DocumentIdentificatieHuidige', 'Het laatste volgnummer dat is gebruikt voor de documentidentificatie in nl.haarlem.translations.zdstozgw.convertor.impl.GenereerDocumentIdentificatie', '1'
 WHERE ( SELECT COUNT(*) FROM emulate_parameter WHERE parameter_name = 'DocumentIdentificatieHuidige') = 0;
 
-CREATE OR REPLACE FUNCTION tekstTussenTekst(zoektekst TEXT, voorTussen TEXT, naTussen TEXT) RETURNS TEXT AS $$
+CREATE OR REPLACE FUNCTION tekstTussenTekst(zoektekst TEXT, voorTussen TEXT, naTussen TEXT) RETURNS TEXT AS '
 DECLARE
-  resultaat text;
-  positie INT;
+	resultaat text;
+	positie INT;
 BEGIN
 	positie := POSITION(voorTussen IN zoektekst);
-  	IF positie = 0 THEN
- 		RETURN NULL;
-  	END IF;	
-	resultaat := SUBSTRING(zoektekst, positie + LENGTH(voorTussen));	
+	IF positie = 0 THEN
+		RETURN NULL;
+	END IF;	
+	resultaat := SUBSTRING(zoektekst, positie + LENGTH(voorTussen));
 	positie := POSITION(naTussen IN resultaat);
-  	IF positie = 0 THEN
- 		RETURN NULL;
-  	END IF;	
-	resultaat := SUBSTRING(resultaat, 0, positie);	
-  	RETURN resultaat;
+	IF positie = 0 THEN
+		RETURN NULL;
+	END IF;	
+	resultaat := SUBSTRING(resultaat, 0, positie);
+	RETURN resultaat;
 END;
-$$ LANGUAGE 'plpgsql'
+' LANGUAGE plpgsql
 
 CREATE OR REPLACE FUNCTION create_sequences_if_not_exists()
 RETURNS VOID AS '
@@ -39,10 +39,10 @@ DECLARE
 	document_id_start_value BIGINT;
 BEGIN
 	SELECT parameter_value INTO zaak_id_start_value FROM emulate_parameter WHERE parameter_name = ''ZaakIdentificatieHuidige'';
-	EXECUTE ''CREATE SEQUENCE HuidigeZaakIdentificatie START WITH '' || zaak_id_start_value + 1;
+	EXECUTE ''CREATE SEQUENCE IF NOT EXISTS HuidigeZaakIdentificatie START WITH '' || zaak_id_start_value + 1;
 
 	SELECT parameter_value INTO document_id_start_value FROM emulate_parameter WHERE parameter_name = ''DocumentIdentificatieHuidige'';
-	EXECUTE ''CREATE SEQUENCE HuidigeDocumentIdentificatie START WITH '' || document_id_start_value + 1;
+	EXECUTE ''CREATE SEQUENCE IF NOT EXISTS HuidigeDocumentIdentificatie START WITH '' || document_id_start_value + 1;
 END;
 ' LANGUAGE plpgsql;
 
